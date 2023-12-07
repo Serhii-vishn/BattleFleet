1025190d864261e9b1642fb33ace5203085b1a26
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2023-12-07 22:09:19 +0200

    Add ahip class

--- BattleFleet/src/Board/Ship.cs
+++ BattleFleet/src/Board/Ship.cs
@@ -6,18 +6,49 @@ namespace BattleFleet.src.Board
     {
         private ShipClass shipClass;
         private Dictionary<char, int> position;
+        private int health;
         private bool isSunk;
 
-        Ship(ShipClass shipClass, Dictionary<char, int> position)
+
+        public Ship(ShipClass shipClass, Dictionary<char, int> position)
         {
             this.shipClass = shipClass;
             this.position = position;
+            health = (int)shipClass;
             this.isSunk = false;
         }
 
-        public void updateStatus()
+        private void UpdateStatus()
         {
             isSunk = true;
         }
+
+        public void hit()
+        {
+            if (health > 0)
+            {
+                int damage = 1;
+                health -= damage;
+
+                if (health == 0)
+                {
+                    UpdateStatus();
+                }
+            }
+            else
+            {
+                throw new ShipAlreadySunkException();
+            }
+        }
+        
+        public int getHealth()
+        {
+            return health;
+        }
     }
+}
+
+public class ShipAlreadySunkException : Exception
+{
+    public ShipAlreadySunkException() : base("Cannot hit a ship that is already sunk."){}
 }
