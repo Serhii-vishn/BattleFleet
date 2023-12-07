5fa3c7ee1a2079a667b4e66214c9873182016bac
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2023-12-07 22:09:48 +0200

    Add Draw board and initialize player board

---
+++ BattleFleet/src/Board/Board.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleFleet.src.Board
+{
+    class Board
+    {
+        private const int kGridLength = 10;
+        private Cell[,] grid;
+        private List<Ship> shipsList;
+        private char[] alphabetCells = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
+
+        public Board()
+        {
+            grid = new Cell[10, 10];
+            InitializeBoard();
+        }
+
+        private void InitializeBoard()
+        {
+            for(int i = 0; i < kGridLength; i++)
+            {
+                for (int j = 0; j < kGridLength; j++)
+                {
+                    grid[i, j] = new Cell(i, alphabetCells[j]);
+                }
+            }
+        }
+
+        public string DrawBoard()
+        {
+            const string horizontalSeparator = "\n---+---+---+---+---+---+---+---+---+---+---+";
+            StringBuilder board = new StringBuilder();
+
+            board.Append(" X |");
+            board.Append(string.Join("", Enumerable.Range(0, kGridLength).Select(i => $" {i} |")));
+
+            for (int i = 0; i < kGridLength; i++)
+            {
+                board.AppendLine(horizontalSeparator);
+                board.Append($" {alphabetCells[i]} |");
+
+                board.Append(string.Join("", Enumerable.Range(0, kGridLength).Select(j => grid[i, j].GetCellCode())));
+            }
+
+            return board.ToString();
+        }
+
+        //private void CreateShips()
+        //{
+        //    shipsList.Add(new Ship())
+        //}
+        //public bool placeShip(Ship)
+        //{
+
+        //}
+
+    }
+}
