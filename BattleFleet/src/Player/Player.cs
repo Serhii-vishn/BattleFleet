d4e03ddc7932808933b3e61edb0f051661970668
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2023-12-20 21:18:25 +0200

    Add player class

---
+++ BattleFleet/src/Player/Player.cs
@@ -0,0 +1,22 @@
+﻿using BattleFleet.src.PlayerBoard;
+using System;
+
+namespace BattleFleet.src.Player
+{
+    abstract class Player
+    {
+        protected string playerName;
+
+        public Player(string playerName)
+        { 
+            this.playerName = playerName; 
+        }
+
+        public string GetPlayerName()
+        { 
+            return playerName; 
+        }
+
+        public abstract void MakeMove(Board opponentBoard);
+    }
+}
