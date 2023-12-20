64d688442535238da72d453632368f9c8c850b78
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2023-12-20 20:27:51 +0200

    added GameConsoleUI class that creates an interface for user interaction

--- BattleFleet/src/UI/GameConsoleUI.cs
+++ BattleFleet/src/UI/GameConsoleUI.cs
@@ -8,8 +8,57 @@ namespace BattleFleet.src.UI
 {
     class GameConsoleUI
     {
+        public int consoleWidth {  get; private set; }
+        public int consoleHeight { get; private set; }
 
-      
+        public GameConsoleUI()
+        {
+            Console.Title = "BattleFleet";
+
+            consoleWidth = 100;
+            consoleHeight = 50;
+
+            Console.SetWindowSize(consoleWidth, consoleHeight);
+            Console.SetBufferSize(consoleWidth, consoleHeight);
+        }
+
+        public void DisplayHeader()
+        {
+            Console.Clear();
+
+            string[] initialMessages = {
+                "           ~```",
+                "       ||",
+                "  /=========\\",
+                "_\\__\\\\____|_________|__//_//_",
+                "\\                           /",
+                " \\    o   o   o   o   o    /",
+                " \\                       /",
+                "~~~|~~~~~~~~~~~~~~~~~~~~~|~~~",
+                " ",
+                "Welcome to the game - BattleFleet",
+                "\t  *By @Serhii-vishn",
+                "\t      **Version 1.1"
+            };
+
+            int centerX = consoleWidth / 2;
+            int centerY = (consoleHeight - initialMessages.Length) / 2;          
+
+            foreach (string message in initialMessages)
+            {
+                Console.SetCursorPosition(centerX - message.Length / 2, centerY++);
+                Console.WriteLine(message);
+            }
+        }
+       
+
+        public void DisplayMainMenu()
+        {
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+
 
     }
 }
