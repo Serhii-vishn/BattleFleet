ba27853c831ae9747632d8ac2eefd38ec237a640
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2023-12-21 14:33:24 +0200

    start menu for tests player

--- BattleFleet/src/Program.cs
+++ BattleFleet/src/Program.cs
@@ -1,4 +1,5 @@
-﻿using BattleFleet.src.PlayerBoard;
+﻿using BattleFleet.src.Player;
+using BattleFleet.src.PlayerBoard;
 using BattleFleet.src.UI;
 
 internal class Program
@@ -7,11 +8,41 @@ internal class Program
     {
         Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-        GameConsoleUI gameConsoleUI = new GameConsoleUI();
-        gameConsoleUI.DisplayHeader();
+        //GameConsoleUI gameConsoleUI = new GameConsoleUI();
+        //gameConsoleUI.DisplayHeader();
 
-        gameConsoleUI.DisplayMainMenu();
+        //gameConsoleUI.DisplayMainMenu();
 
+        Console.Clear();
+        Board b1 = new Board();
+        Board b2 = new Board();
+
+        HumanPlayer humanPlayer = new HumanPlayer("pl1");
+        humanPlayer.Initialize(b1, b2);
+
+
+        do
+        {
+            Console.Clear();
+            humanPlayer.DrawBoard();
+
+            try
+            {
+                humanPlayer.PlaceShips();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"\nError: {ex.Message}");
+            }
+
+            Console.Write("Do you want to place another ship? (Y/N): ");
+
+        } while (Console.ReadKey().Key == ConsoleKey.Y);
+
+
+        Console.ReadKey();
+
+        // humanPlayer.MakeMove();
         Console.ReadKey();
     }
 }
