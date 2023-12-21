df1c83174a69662a3d2179feba13829761a57bb0
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2023-12-21 12:30:12 +0200

    added a class that handles the computer's progress

---
+++ BattleFleet/src/Player/ComputerPlayer.cs
@@ -0,0 +1,23 @@
+﻿using BattleFleet.src.PlayerBoard;
+using System;
+
+namespace BattleFleet.src.Player
+{
+    class ComputerPlayer : Player
+    {
+        public ComputerPlayer(string playerName) : base(playerName) { }
+
+        public override void Initialize(Board ownBoard, Board opponentBoard)
+        {
+            this.ownBoard = ownBoard;
+            this.opponentBoard = opponentBoard;
+        }
+
+        public override void MakeMove()
+        {
+
+            // Логіка для автоматичного визначення координат ходу комп'ютера
+            // Обробка ходу та оновлення дошки
+        }
+    }
+}
