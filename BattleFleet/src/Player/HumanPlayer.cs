936de889b9dec0bb113ed0a24b45b759f0a79ce5
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2023-12-21 12:30:22 +0200

    added a class that handles the player's move

---
+++ BattleFleet/src/Player/HumanPlayer.cs
@@ -0,0 +1,25 @@
+﻿using BattleFleet.src.PlayerBoard;
+using System;
+
+namespace BattleFleet.src.Player
+{
+    class HumanPlayer : Player
+    {
+        public HumanPlayer(string playerName) : base(playerName) { }
+
+        public override void Initialize(Board ownBoard, Board opponentBoard)
+        {
+            this.ownBoard = ownBoard;
+            this.opponentBoard = opponentBoard;
+        }
+
+        public override void MakeMove()
+        {
+            Console.WriteLine($"Хід гравця {playerName}.");
+
+            // Логіка для введення координат від гравця через консоль
+            // Обробка ходу та оновлення дошки
+        }
+    }
+
+}
