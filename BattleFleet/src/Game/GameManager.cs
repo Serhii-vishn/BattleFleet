b469220efbc499d16dc894f7acc1eb8204d56355
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2023-12-26 14:35:11 +0200

    Added gameManager methods

--- BattleFleet/src/Game/GameManager.cs
+++ BattleFleet/src/Game/GameManager.cs
@@ -1,22 +1,45 @@
 ﻿using System;
 
-
 namespace BattleFleet.src.Game
 {
+    using BattleFleet.src.Player;
     class GameManager
     {
-        private Game game;
+        private Player player1;
+        private Player player2;
 
-        //public GameManager(Player player1, Player player2)
-    
-        //public Game InitializeGame()
-    
-        //private void ConfigureGameRules()
-    
-        //private void SetupPlayers()
-    
-        //private void SetupBoards()
-    
-        //public void DisplayGameStatistics(Game game)
+        public GameManager(Player player1, Player player2)
+        {
+            this.player1 = player1;
+            this.player2 = player2;
+        }
+
+        public Game InitializeGame()
+        {
+            SetupPlayers();
+            Console.WriteLine("The sea battle game has started!");
+            Console.WriteLine($"Player 1: {player1.GetPlayerName()}");
+            Console.WriteLine($"Player 2: {player2.GetPlayerName()}");
+
+            Game game = new Game(player1, player2);
+
+            return game;
+        }
+
+        private void SetupPlayers()
+        {
+            Console.Write("Enter Player 1 name: ");
+            string player1Name = Console.ReadLine();
+            player1.SetPlayerName(player1Name);
+
+            Console.Write("Enter Player 2 name: ");
+            string player2Name = Console.ReadLine();
+            player2.SetPlayerName(player2Name);
+        }
+
+        public void DisplayGameStatistics(Game game)
+        {
+            //
+        }
     }
 }
