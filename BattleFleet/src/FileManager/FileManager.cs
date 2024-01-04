fea78a215c2948f4d1ed99ff48ad57c3f78469fe
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-01-04 13:00:01 +0200

    created a parent class to manage directories and file paths

---
+++ BattleFleet/src/FileManager/FileManager.cs
@@ -0,0 +1,49 @@
+﻿namespace BattleFleet.src.FileManager
+{
+    class FileManager
+    {
+        protected string mainFolderPath;
+        protected string logsFolderPath;
+        protected string savesFolderPath;
+
+        public FileManager()
+        {
+            mainFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "resources");
+            logsFolderPath = Path.Combine(mainFolderPath, "logs");
+            savesFolderPath = Path.Combine(mainFolderPath, "saves");
+
+            ensureDirectoryExists(mainFolderPath);
+            ensureDirectoryExists(logsFolderPath);
+            ensureDirectoryExists(savesFolderPath);
+        }
+
+        private void ensureDirectoryExists (string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                try
+                {
+                    Directory.CreateDirectory(directoryPath);
+                }
+                catch (Exception ex)
+                {
+                    throw new ArgumentException($"Error creating directory {directoryPath}: {ex.Message}");
+                }
+            }
+        }
+
+        protected string GetFilePathInMainFolder(string fileName)
+        {
+            string filePath = Path.Combine(mainFolderPath, fileName);
+
+            if (File.Exists(filePath))
+            {
+                return filePath;
+            }
+            else
+            {
+                throw new ArgumentException($"File '{fileName}' does not exist.");
+            }
+        }
+    }
+}
