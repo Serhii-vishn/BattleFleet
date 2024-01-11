8143626eeceb09b1886f3aae2afadc7aa0c69388
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-01-11 22:56:03 +0200

    code optimization added tests

---
+++ BattleFleet.Tests/ShipTemplateManagerTests.cs
@@ -0,0 +1,33 @@
+﻿using BattleFleet.src.FileManager;
+
+namespace BattleFleet.Tests
+{
+    [TestClass]
+    public class ShipTemplateManagerTests
+    {
+        [TestMethod]
+        public void GetTemplateNames_ReturnsTemplateNames()
+        {
+            ShipTemplateManager templateManager = new ShipTemplateManager();
+
+            List<string> templateNames = templateManager.GetTemplateNames();
+
+            CollectionAssert.Contains(templateNames, "Master");
+            CollectionAssert.Contains(templateNames, "Arcade");
+            CollectionAssert.Contains(templateNames, "Redis");
+            CollectionAssert.Contains(templateNames, "GLORY_FOR_UKRAINE");
+        }
+
+        [TestMethod]
+        public void GetTemplateByName_ReturnsCorrectTemplate()
+        {
+            ShipTemplateManager templateManager = new ShipTemplateManager();
+
+            List<string> template = templateManager.GetTemplateByName("Master");
+
+            Assert.IsNotNull(template);
+            CollectionAssert.Contains(template, "5,A,FIVE_DECK,VERTICAL");
+            CollectionAssert.Contains(template, "8,D,ONE_DECK,HORIZONTAL");
+        }
+    }
+}
--- BattleFleet/src/FileManager/ShipTemplateManager.cs
+++ BattleFleet/src/FileManager/ShipTemplateManager.cs
@@ -1,6 +1,6 @@
 ﻿namespace BattleFleet.src.FileManager
 {
-    class ShipTemplateManager : FileManager
+    public class ShipTemplateManager : FileManager
     {
         private readonly List<string> templates;
         private readonly string templatesFilePath;
@@ -12,6 +12,88 @@
 
             ensureFileExists (templatesFilePath);
             ensureFileNotEmpty (templatesFilePath);
+        }
+
+        public void SaveTemplate(List<string> template, string templateName)
+        {
+            try
+            {
+                using (FileStream fileStream = new FileStream(templatesFilePath, FileMode.Append, FileAccess.Write))
+                using (StreamWriter writer = new StreamWriter(fileStream))
+                {
+                    writer.WriteLine("\nName: " + templateName);
+                    foreach (var str in template)
+                    {
+                        writer.WriteLine(str);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Error saving chat history: " + ex.Message);
+            }
+        }
+
+        public List<string> GetTemplateNames()
+        {
+            var templateNames = new List<string>();
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(templatesFilePath))
+                {
+                    string line;
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line.StartsWith("Name:"))
+                        {
+                            templateNames.Add(line.Replace("Name:", "").Trim());
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading template names: " + ex.Message);
+            }
+
+            return templateNames;
+        }
+
+        public List<string> GetTemplateByName(string templateName)
+        {
+            var template = new List<string>();
+            try
+            {
+                using (StreamReader reader = new StreamReader(templatesFilePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line.StartsWith("Name:") && line.Replace("Name:", "").Trim() == templateName)
+                        {
+                            while ((line = reader.ReadLine()) != null)
+                            {
+                                if (line.StartsWith("Name:"))
+                                {
+                                    break;
+                                }
+                                if (!string.IsNullOrWhiteSpace(line))
+                                {
+                                    template.Add(line);
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading template: " + ex.Message);
+            }
+            return template;
         }
 
         private void ensureFileExists (string path)
@@ -97,88 +179,6 @@
                 });
                 SaveTemplate(templates, tmplName);
             }
-        }
-
-        public void SaveTemplate(List<string> template, string templateName)
-        {
-            try
-            {
-                using (FileStream fileStream = new FileStream(templatesFilePath, FileMode.Append, FileAccess.Write))
-                using (StreamWriter writer = new StreamWriter(fileStream))
-                {
-                    writer.WriteLine("\nName: " + templateName);
-                    foreach (var str in template)
-                    {
-                        writer.WriteLine(str);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new ArgumentException("Error saving chat history: " + ex.Message);
-            }
-        }
-
-        public List<string> GetTemplateNames()
-        {
-            var templateNames = new List<string>();
-
-            try
-            {
-                using (StreamReader reader = new StreamReader(templatesFilePath))
-                {
-                    string line;
-
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        if (line.StartsWith("Name:"))
-                        {
-                            templateNames.Add(line.Replace("Name:", "").Trim());
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error reading template names: " + ex.Message);
-            }
-
-            return templateNames;
-        }
-
-        public List<string> GetTemplateByName(string templateName)
-        {
-            var template = new List<string>();
-            try
-            {
-                using (StreamReader reader = new StreamReader(templatesFilePath))
-                {
-                    string line;
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        if (line.StartsWith("Name:") && line.Replace("Name:", "").Trim() == templateName)
-                        {
-                            while ((line = reader.ReadLine()) != null)
-                            {
-                                if (line.StartsWith("Name:"))
-                                {
-                                    break;
-                                }
-                                if (!string.IsNullOrWhiteSpace(line))
-                                {
-                                    template.Add(line);
-                                }
-                            }
-                            break;
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error reading template: " + ex.Message);
-            }
-            return template;
         }
     }
 }
