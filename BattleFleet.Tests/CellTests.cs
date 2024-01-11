8a82cbb877ce650788d2d8d0721a6cbf1ef9aa7d
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-01-11 21:28:29 +0200

    tests: added tests for Cell class

---
+++ BattleFleet.Tests/CellTests.cs
@@ -0,0 +1,73 @@
+﻿using BattleFleet.src.PlayerBoard;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+
+namespace BattleFleet.Tests
+{
+    [TestClass]
+    public sealed class CellTests
+    {
+        private readonly Cell cell = new Cell();
+
+        [TestMethod]
+        public void CreateCellAndCheckInitialValue()
+        {
+            CellStatus cellStatus = cell.GetCellStatus();
+
+            Assert.AreEqual(CellStatus.EMPTY, cellStatus);
+        }
+
+        [TestMethod]
+        [DataRow(CellStatus.EMPTY, true)]
+        [DataRow(CellStatus.OCCUPIED, true)]
+        [DataRow(CellStatus.FORBIDDEN, true)]
+        public void UpdateCellAndCheck(CellStatus newStatusCell, bool expectedResult)
+        {
+            bool result = cell.UpdateCellStatus(newStatusCell);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        [DataRow(CellStatus.HIT, CellStatus.HIT, false)]
+        [DataRow(CellStatus.MISS, CellStatus.HIT, true)]
+        public void SetCellStatusAndHitCell(CellStatus startStatusCell, CellStatus newStatusCell, bool expectedResult)
+        {
+            cell.UpdateCellStatus(startStatusCell);
+
+            bool result = cell.UpdateCellStatus(newStatusCell);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        [DataRow(CellStatus.EMPTY, $"   |")]
+        [DataRow(CellStatus.OCCUPIED, $"   |")]
+        [DataRow(CellStatus.FORBIDDEN, $"   |")]
+        [DataRow(CellStatus.HIT, $" X |")]
+        [DataRow(CellStatus.MISS, $" - |")]
+        public void SetCellStatusAndGetHideCell(CellStatus startStatusCell, string expectedResult)
+        {
+            cell.UpdateCellStatus(startStatusCell);
+
+            string result = cell.ToStringHide();
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        [DataRow(CellStatus.EMPTY, $"   |")]
+        [DataRow(CellStatus.OCCUPIED, $" ■ |")]
+        [DataRow(CellStatus.FORBIDDEN, $"   |")]
+        [DataRow(CellStatus.HIT, $" X |")]
+        [DataRow(CellStatus.MISS, $" - |")]
+        public void SetCellStatusAndGetCodeCell(CellStatus startStatusCell, string expectedResult)
+        {
+            cell.UpdateCellStatus(startStatusCell);
+
+            string result = cell.ToString();
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
